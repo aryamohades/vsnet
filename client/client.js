fdd14088700171182e69a4333809655269b8194c
arya <aryamohades@gmail.com>
2019-12-27 06:01:09 -0600

    add scripts

--- client/client.js
+++ client/client.js
@@ -1,4 +1,5 @@
 require('log-timestamp');
+
 const WebSocket = require('ws');
 const argv = require('yargs').argv
 
@@ -9,28 +10,29 @@ const {
   port = 8000
 } = argv;
 
+const ab = new ArrayBuffer();
 const url = `ws://${ip}:${port}/ws`;
 
 for (let i = 0; i < conn; i++) {
   const ws = new WebSocket(url);
 
   ws.on('open', () => {
-    // console.log(`established connection ${i+1}`);
+    console.log(`established connection ${i+1}`);
 
-    // setInterval(() => {
-    //   ws.send(`hello from connection ${i+1}`);
-    // }, 1000);
+    setInterval(() => {
+      ws.send(ab);
+    }, 1000);
   });
   
   ws.on('close', () => {
-    console.log(`connection ${i} disconnected`);
+    console.log(`connection ${i+1} disconnected`);
   });
 
   ws.on('error', error => {
-    console.log(`connection ${i} error: ${error.message}`)
+    console.log(`connection ${i+1} error: ${error.message}`)
   });
 
   ws.on('message', data => {
-    // console.log(`received message from server: ${data}`);
+    console.log(`received message from server: ${data}`);
   });
 }
--- wss/src/server.js
+++ wss/src/server.js
@@ -3,43 +3,47 @@ const uWS = require('uWebSockets.js');
 const port = 8000;
 let counter = 0;
 
-const app = uWS.App().ws('/ws', {
-  /* Options */
-  compression: 0,
-  maxPayloadLength: 16 * 1024 * 1024,
-  idleTimeout: 10,
-  /* Handlers */
-  open: (ws, req) => {
-    counter++;
+const app = uWS
+  .App()
+  .ws('/ws', {
+    /* Options */
+    compression: 0,
+    maxPayloadLength: 16 * 1024 * 1024,
+    idleTimeout: 30,
+    /* Handlers */
+    open: (ws, req) => {
+      counter++;
 
-    if (counter % 100 === 0) {
+      if (counter % 100 === 0) {
         console.log(`connections: ${counter}`);
-    }
+      }
 
-    // console.log('A WebSocket connected via URL: ' + req.getUrl() + '!');
-  },
-  message: (ws, message, isBinary) => {
-    /* Ok is false if backpressure was built up, wait for drain */
-    let ok = ws.send(message, isBinary);
-  },
-  drain: (ws) => {
-    console.log('WebSocket backpressure: ' + ws.getBufferedAmount());
-  },
-  close: (ws, code, message) => {
-    counter--;
+      console.log(`a client connected via URL: ${req.getUrl()}`);
+    },
+    message: (ws, message, isBinary) => {
+      /* Ok is false if backpressure was built up, wait for drain */
+      let ok = ws.send(message, isBinary);
+    },
+    drain: (ws) => {
+      console.log(`socket backpressure: ${ws.getBufferedAmount()}`);
+    },
+    close: (ws, code, message) => {
+      counter--;
 
-    if (counter % 100 === 0) {
+      if (counter % 100 === 0) {
         console.log(`connections: ${counter}`);
-    }
+      }
 
-    // console.log('WebSocket closed');
-  }
-}).any('/*', (res, req) => {
-  res.end('Nothing to see here!');
-}).listen(port, (token) => {
-  if (token) {
-    console.log('Listening on port ' + port);
-  } else {
-    console.log('Failed to listen to port ' + port);
-  }
-});
+      console.log('socket closed');
+    }
+  })
+  .any('/*', (res, req) => {
+    res.end('nothing to see here!');
+  })
+  .listen(port, (token) => {
+    if (token) {
+      console.log(`listening on port ${port}`);
+    } else {
+      console.log(`failed to listen to port ${port}`);
+    }
+  });
