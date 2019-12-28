01d6719246459c69cc34c01c1ede4506cbcb6351
arya <aryamohades@gmail.com>
2019-12-27 19:57:06 -0600

    example monorepo setup with pnpm

--- client/src/client.js
+++ client/src/client.js
@@ -1,14 +1,9 @@
 require('log-timestamp');
 
 const WebSocket = require('ws');
-const argv = require('yargs').argv
+const argv = require('yargs').argv;
 
-const {
-  conn = 1,
-  ramp = 0,
-  ip = '127.0.0.1',
-  port = 8000
-} = argv;
+const { conn = 1, ramp = 0, ip = '127.0.0.1', port = 8000 } = argv;
 
 const ab = new ArrayBuffer();
 const url = `ws://${ip}:${port}/ws`;
@@ -17,22 +12,22 @@ for (let i = 0; i < conn; i++) {
   const ws = new WebSocket(url);
 
   ws.on('open', () => {
-    console.log(`established connection ${i+1}`);
+    console.log(`established connection ${i + 1}`);
 
     setInterval(() => {
       ws.send(ab);
     }, 1000);
   });
-  
+
   ws.on('close', () => {
-    console.log(`connection ${i+1} disconnected`);
+    console.log(`connection ${i + 1} disconnected`);
   });
 
-  ws.on('error', error => {
-    console.log(`connection ${i+1} error: ${error.message}`)
+  ws.on('error', (error) => {
+    console.log(`connection ${i + 1} error: ${error.message}`);
   });
 
-  ws.on('message', data => {
+  ws.on('message', (data) => {
     console.log(`received message from server: ${data}`);
   });
 }
